48c8a338eb7842f8295f6d82288c3bff2e883fc2
Plunne <plunne.celeste@gmail.com>
2022-08-26 15:46:23 +0200

    Zapette v0.21

--- zapette_v0_2/zapette_v0_2.ino
+++ zapette_v0_2/zapette_v0_2.ino
@@ -17,11 +17,49 @@
 
 #define USE_NIMBLE
 
-/* Diodes */
-#define LED_BLE 			2
+/* Keys */
 
-/* Media keys */
-#define MEDIA_PLAYPAUSE 	15
+// Laser
+#define LASER_SW			27
+
+// Media keys
+#define MEDIA_PLAY			15
+
+/* Design v0.3
+MEDIA_PREV			2 <- pulldown only
+MEDIA_NEXT			12 <- pulldown only
+*/
+
+#define MEDIA_VOL_UP		19
+#define MEDIA_VOL_DOWN		18
+#define MEDIA_VOL_MUTE		5
+
+/* Design v0.3
+MEDIA_VOL_UP    32
+MEDIA_VOL_DOWN    33
+MEDIA_VOL_MUTE    25 <- new in replacement of GPIO35 external pullup needed
+*/
+
+// Arrow keys
+// UP_ARROW			0 <- cannot be used (boot reserved)
+#define DOWN_ARROW			17
+#define LEFT_ARROW			16
+#define RIGHT_ARROW			26
+#define OK_RETURN			4
+
+// Switch keys
+#define SW_KEY1        32
+#define SW_KEY2       33
+
+/* Design v0.3
+SW_KEY1				19
+SW_KEY2				18
+SW_KEY3				5
+*/
+
+/* Diodes */
+#define LED_BLE 			13
+#define LASER_DIODE 		14
 
 /* Zapette object setup */
 BleKeyboard Zapette("Zapette BLE", "PlunneCeleste", 100);
@@ -39,10 +77,28 @@ void setup() {
 	Zapette.begin();
 
 	/* INPUTS */
-    pinMode(MEDIA_PLAYPAUSE, INPUT_PULLUP);
+    pinMode(LASER_SW, INPUT_PULLUP);
+	
+    pinMode(MEDIA_PLAY, INPUT_PULLUP);
+    // pinMode(MEDIA_PREV, INPUT);
+    // pinMode(MEDIA_NEXT, INPUT);
+    pinMode(MEDIA_VOL_UP, INPUT_PULLUP);
+    pinMode(MEDIA_VOL_DOWN, INPUT_PULLUP);
+    pinMode(MEDIA_VOL_MUTE, INPUT_PULLUP);
+
+	// pinMode(UP_ARROW, INPUT_PULLUP);
+    pinMode(DOWN_ARROW, INPUT_PULLUP);
+    pinMode(LEFT_ARROW, INPUT_PULLUP);
+    pinMode(RIGHT_ARROW, INPUT_PULLUP);
+    pinMode(OK_RETURN, INPUT_PULLUP);
+
+	pinMode(SW_KEY1, INPUT_PULLUP);
+	pinMode(SW_KEY2, INPUT_PULLUP);
+	//pinMode(SW_KEY3, INPUT_PULLUP); // v0.3
 	
 	/* OUTPUTS */
     pinMode(LED_BLE, OUTPUT);
+    pinMode(LASER_DIODE, OUTPUT);
 }
 
 /****************
@@ -52,9 +108,33 @@ void setup() {
 void loop() {
 
     if(Zapette.isConnected())
-	{	
+	{
+		if(!digitalRead(LASER_SW)) {
+			digitalWrite(LASER_DIODE, HIGH);
+		} else {
+			digitalWrite(LASER_DIODE, LOW);
+		}
 		/* Media Keys */
-       	sendMediaToBLE(MEDIA_PLAYPAUSE, KEY_MEDIA_PLAY_PAUSE); 
+       	sendMediaToBLE(MEDIA_PLAY, KEY_MEDIA_PLAY_PAUSE); 
+       	// sendMediaPullDownToBLE(MEDIA_PREV, KEY_MEDIA_PREVIOUS_TRACK); 
+       	// sendMediaPullDownToBLE(MEDIA_NEXT, KEY_MEDIA_NEXT_TRACK); 
+       	sendMediaToBLE(MEDIA_VOL_UP, KEY_MEDIA_VOLUME_UP); 
+       	sendMediaToBLE(MEDIA_VOL_DOWN, KEY_MEDIA_VOLUME_DOWN); 
+       	sendMediaToBLE(MEDIA_VOL_MUTE, KEY_MEDIA_MUTE); 
+		/* Normal Keys */
+       	// sendKeyToBLE(UP_ARROW, KEY_UP_ARROW); 
+       	sendKeyToBLE(DOWN_ARROW, KEY_DOWN_ARROW); 
+       	sendKeyToBLE(LEFT_ARROW, KEY_LEFT_ARROW); 
+       	sendKeyToBLE(RIGHT_ARROW, KEY_RIGHT_ARROW); 
+       	sendKeyToBLE(OK_RETURN, KEY_RETURN);
+		/* Switch keys */
+		sendKeyToBLE(SW_KEY1, 'f');
+		sendKeyToBLE(SW_KEY2, KEY_F5);
+		// sendKeyToBLE(SW_KEY3, KEY_ESC);
+		delay(150);
+		Zapette.releaseAll();
+		digitalWrite(LED_BLE, LOW);  // BLE LED = OFF
+		
     }
 	else
 	{
@@ -68,6 +148,8 @@ void loop() {
 		digitalWrite(LED_BLE, LOW);
 		delay(2500);
 	}
+
+	
 }
 
 /********************
@@ -80,10 +162,7 @@ void sendKeyToBLE(uint8_t pin, uint8_t ascii)
 {
 	if (!digitalRead(pin)) { 			// Key pressed
 		digitalWrite(LED_BLE, HIGH); 	// BLE LED = ON
-		Zapette.write(ascii); 			// Send NORMAL key to BLE
-		while (!digitalRead(pin)); 		// While key pressed do nothing
-		delay(250); 					// 250ms Delay to avoid debounce
-		digitalWrite(LED_BLE, LOW); 	// BLE LED = OFF
+		Zapette.press(ascii); 			// Send NORMAL key to BLE
 	}
 }
 
@@ -92,9 +171,15 @@ void sendMediaToBLE(uint8_t pin, const MediaKeyReport media)
 {
 	if (!digitalRead(pin)) { 			// Key pressed
 		digitalWrite(LED_BLE, HIGH); 	// BLE LED = ON
-		Zapette.write(media); 			// Send MEDIA key to BLE
-		while (!digitalRead(pin)); 		// While key pressed do nothing
-		delay(250); 					// 250ms Delay to avoid debounce
-		digitalWrite(LED_BLE, LOW); 	// BLE LED = OFF
+		Zapette.press(media); 			// Send MEDIA key to BLE
+	}
+}
+
+/* Maybe v0.3 */
+void sendMediaPullDownToBLE(uint8_t pin, const MediaKeyReport media)
+{
+	if (digitalRead(pin)) { 			// Key pressed
+		digitalWrite(LED_BLE, HIGH); 	// BLE LED = ON
+		Zapette.press(media); 			// Send MEDIA key to BLE
 	}
 }
